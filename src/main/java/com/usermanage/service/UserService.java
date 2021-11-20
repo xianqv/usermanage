9a3d352cbaa5dbe2d4d462a208d178074613f047
one <one@qq.com>
2021-11-20 11:03:19 +0800

    service

--- src/main/java/com/usermanage/service/UserService.java
+++ src/main/java/com/usermanage/service/UserService.java
@@ -1,4 +1,40 @@
 package com.usermanage.service;
 
+import com.usermanage.domain.UserBean;
+
+import java.util.List;
+
+/***
+ * 用户管理的业务接口
+ */
 public interface UserService {
+    /**
+     * 查询所有用户
+     * @return
+     */
+    public List<UserBean> findAll();
+
+    /***
+     * 根据id查询用户
+     * @return
+     */
+    public UserBean findById(int id );
+
+    /***
+     * 新增用户
+     * @param user
+     */
+    public  void addUser(UserBean user);
+
+    /***
+     * 用户信息更新
+     * @param user
+     */
+    public void updateUser(UserBean user);
+
+    /***
+     * 删除用户
+     * @param id
+     */
+    public void delUser(String id);
 }
--- src/main/java/com/usermanage/service/impl/UserServiceImpl.java
+++ src/main/java/com/usermanage/service/impl/UserServiceImpl.java
@@ -1,6 +1,58 @@
 package com.usermanage.service.impl;
 
+import com.usermanage.dao.UserDao;
+import com.usermanage.dao.impl.UserDaoImpl;
+import com.usermanage.domain.UserBean;
 import com.usermanage.service.UserService;
 
+import java.util.List;
+
 public class UserServiceImpl implements UserService {
+    UserDao userDao=new UserDaoImpl();
+    /**
+     * 查询所有用户
+     *
+     * @return
+     */
+    @Override
+    public List<UserBean> findAll() {
+        return userDao.findAll();
+    }
+
+    /***
+     * 根据id查询用户
+     * @return
+     * @param id
+     */
+    @Override
+    public UserBean findById(int id) {
+        return userDao.findById(id);
+    }
+
+    /***
+     * 新增用户
+     * @param user
+     */
+    @Override
+    public void addUser(UserBean user) {
+        userDao.addUser(user);
+    }
+
+    /***
+     * 用户信息更新
+     * @param user
+     */
+    @Override
+    public void updateUser(UserBean user) {
+        userDao.updateUser(user);
+    }
+
+    /***
+     * 删除用户
+     * @param id
+     */
+    @Override
+    public void delUser(String id) {
+    userDao.delUser(Integer.parseInt(id));
+    }
 }
